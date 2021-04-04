f1d0941a207c9de78d1dced6eed25ed4b68aa699
Vadim Egorov <vadegor00@gmail.com>
2021-04-04 08:09:12 -0800

    first commit

---
+++ src/timer.js
@@ -0,0 +1,25 @@
+
+import './App.css';
+
+function Timer() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <div>
+            yo
+            sdfsdfsd
+            sdf
+            sd
+            fsd
+            f
+            sdf
+            ssdf
+        </div>
+      </header>
+
+      
+    </div>
+  );
+}
+
+export default Timer;
